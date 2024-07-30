69aa0f3009e40fb4884642b6754f0998e837bd41
MountainMan12 <pawan.verma@elucidata.io>
2024-07-30 14:11:55 +0530

    updated script

--- etl/sql/admissions.csv_to_visit_detail.sql
+++ etl/sql/admissions.csv_to_visit_detail.sql
@@ -25,52 +25,19 @@ SELECT
     a.hadm_id AS visit_detail_id,
     a.subject_id AS person_id,
     a.admission_type AS visit_detail_concept_id,
-
- -- [MAPPING   LOGIC] DATETIME to DATE 
     date(a.admittime) AS visit_detail_start_date,
     a.admittime AS visit_detail_start_datetime,
-
- -- [MAPPING   LOGIC] DATETIME to DATE 
     date(a.dischtime) AS visit_detail_end_date,
     a.dischtime AS visit_detail_end_datetime,
-
     NULL AS visit_detail_type_concept_id,
     NULL AS provider_id,
     NULL AS care_site_id,
     a.admission_type AS visit_detail_source_value,
     NULL AS visit_detail_source_concept_id,
-
-	CASE UPPER(a.admission_location)
-		when 'EMERGENCY ROOM' then 4176269
-		when 'PHYSICIAN REFERRAL' then 45884373
-		when 'TRANSFER FROM HOSPITAL' then 44790567
-		when 'WALK-IN/SELF REFERRAL' then 44805382
-		when 'CLINIC REFERRAL' then 4081947
-		when 'PROCEDURE SITE' then 4181646
-		when 'PACU' then 40569634
-		when 'TRANSFER FROM SKILLED NURSING FACILITY' then 4164912
-		when 'INTERNAL TRANSFER TO OR FROM PSYCH' then 0
-		when 'INFORMATION NOT AVAILABLE' then 21498751
-		when 'AMBULATORY SURGERY TRANSFER' then 21498482
-	ELSE 0 END AS admitted_from_concept_id,
+	0 AS admitted_from_concept_id,
     a.admission_location AS admitted_from_source_value,
     a.discharge_location AS discharged_to_source_value,
-	CASE UPPER(a.discharge_location) 
-		when 'HOME' then 4139502
-		when 'HOME HEALTH CARE' then 762451
-		when 'SKILLED NURSING FACILITY' then 706296
-		when 'REHAB' then 
-		when 'DIED' then
-		when 'CHRONIC/LONG TERM ACUTE CARE' then
-		when 'ACUTE HOSPITAL' then
-		when 'HOSPICE' then
-		when 'PSYCH FACILITY' then
-		when 'AGAINST ADVICE' then
-		when 'OTHER FACILITY' then
-		when 'ASSISTED LIVING' then
-		when 'HEALTHCARE FACILITY' then
-	ELSE 0 END AS discharged_to_concept_id,
-
+	0 AS discharged_to_concept_id,
     NULL AS preceding_visit_detail_id,
     NULL AS parent_visit_detail_id,
     NULL AS visit_occurrence_id
